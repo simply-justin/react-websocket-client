d5a34e4373f0ff10769ce8989878e64dde5e6e7f
Justin <justinvdmey@hotmail.com>
2024-12-16 22:12:17 +0100

    Fixed typed export

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
+    format: ['cjs', 'esm'],
     entry: ['src/index.ts'],
-    splitting: false,
-    sourcemap: true,
     clean: true,
+    dts: true,
+    minify: true
 })
